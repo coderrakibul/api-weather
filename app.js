c4c43937c3e6702050ce240224e70a97e290718a
coderrakibul <coder.rakibul@gmail.com>
2022-03-02 20:37:57 +0600

    site is ready

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const searchCity = () => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_key}&units=metric`
    fetch(url)
    .then(response => response.json())
-   .then(data => displayWeather(data))
+   .then(data => displayWeather(data));
+   document.getElementById("city-name").value = "";
 }
 
 const displayWeather = (weather) => {
    document.getElementById("city").innerText = `${weather.name}`;
    document.getElementById("city-temp").innerText = `${weather.main.temp}`;
    document.getElementById("weather-type").innerText = `${weather.weather[0].main}`;
-
 
    //set weather icon
    const url = `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
