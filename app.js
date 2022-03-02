2691024b5a9a82f19aec5a78fd533204693845f1
coderrakibul <coder.rakibul@gmail.com>
2022-03-02 18:50:04 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const API_key = `73a39c0e0e89ce4466183492d8c6dc3d`;
+const searchCity = () => {
+    const cityName = document.getElementById("city-name").value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_key}&units=metric`
+   fetch(url)
+   .then(response => response.json())
+   .then(data => displayWeather(data))
+}
+
+const displayWeather = (weather) => {
+    console.log(weather);
+   document.getElementById("city").innerText = `${weather.name}`;
+   document.getElementById("city-temp").innerText = `${weather.main.temp}`;
+   document.getElementById("weather-type").innerText = `${weather.weather[0].main}`;
+
+}
